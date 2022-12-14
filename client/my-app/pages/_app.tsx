83b61fd31f49d957519b04d833491e9cdc449c26
yahya benamira <yahyabenamira12@.gmail.com>
2022-12-14 11:28:34 -0800

    user schema

--- client/my-app/pages/_app.tsx
+++ client/my-app/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+import '../styles/main.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
