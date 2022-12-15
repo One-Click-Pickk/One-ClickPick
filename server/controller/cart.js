5b0ab915b54730d7ec2bf60e9fb9e147418fc165
Saif-Ayari <saifayari312@gmail.com>
2022-12-15 12:18:39 +0100

    get cart

--- server/controller/cart.js
+++ server/controller/cart.js
@@ -1,5 +1,15 @@
 const Cart = require('../model/cart');
 
-const getAllCarts = () => {};
+const getAllCarts = (req,res) => {
+  Cart.find()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res
+        .status(404)
+        .json({ message: 'Cart not found', error: err.message });
+    });
+};
 
 module.exports = { getAllCarts };
