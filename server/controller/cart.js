971808f6126111ce7ae93b302d5efaaf0f024be9
Saif-Ayari <saifayari312@gmail.com>
2022-12-15 13:51:15 +0100

    add cart

--- server/controller/cart.js
+++ server/controller/cart.js
@@ -19,7 +19,16 @@ const getUserCart = (req, res) => {
       res.status(404).json({ message: 'Cart not found', error: err.message });
     });
 };
-const deleteCart =()=>{
+const addCart=(req,res)=>{
+    Cart.create(req.body)
+    .then((data) => {
+        res.status(201).json({ message: 'successfuly added', data });
+      })
+      .catch((err) => {
+        res.status(400).json({ message: 'failed add', error: err.message });
+      });
+}
+const deleteCart =(req,res)=>{
     Cart.findByIdAndDelete(req.params.id)
     .then((data) => {
         res.status(200).json(data);
@@ -29,4 +38,4 @@ const deleteCart =()=>{
       });
 }
 
-module.exports = { getAllCarts, getUserCart ,deleteCart};
+module.exports = { getAllCarts,addCart, getUserCart ,deleteCart};
